116a17cec90c1d5b01278d3444584210e02a94cf
MJ <maljuboori91@gmail.com>
2015-09-20 12:20:00 -0600

    setup module files

--- module/Pizza/module.php
+++ module/Pizza/module.php
@@ -1,8 +1,19 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+namespace Pizza;
 
+class Module {
+    
+    public function getAutoloaderConfig()
+    {
+        return array(
+            'Zend\Loader\ClassMapAutoloader' => array(__DIR__ . 'autoload_classmap.php',),
+            'Zend\Loader\ClassMapAutoloader' => array('namespace' => array(__DIR__ . '/src/' . __NAMESPACE__,),),
+            );
+    }
+    
+    public function getConfig()
+    {
+      return include __DIR__ . '/config/module.config.php';
+    }
+}
