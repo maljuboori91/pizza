6cd01984c48356a8b84f7c282f535c01171ae3fa
MJ <maljuboori91@gmail.com>
2015-10-19 22:44:06 -0600

    Users: add new changes

--- module/Users/config/module.config.php
+++ module/Users/config/module.config.php
@@ -11,4 +11,25 @@ return array(
             'users' => __DIR__ . '/../view',
         ),
     ),
+    
+    'router' => array(
+        'routes' => array(
+            'users' => array(
+                'type' => 'segment',
+                'options' => array(
+                    'route' => 'users[/:action][/:id]',
+                    'constraints' => array(
+                        'action' => '[a-zA-z][a-zA-Z0-9_-]*',
+                        'id'     => '[0-9]+',
+                    ),
+                ),
+            ),
+        ),
+    ),
+    
+    'view_manager' => array(
+        'template_path_stack' => array(
+            'users' => __DIR__ . '/../view',
+        )
+    )
 );
